0ca1bddc70e33b7e0f39361ee7d919fe1831db5f
tokytorch <famroon1029384756@gmail.com>
2025-10-21 09:27:21 +0300

    Add files via upload

---
+++ omework8/main.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	task3();
+}
+int task3()
+{
+	int N;
+	double sum = 0.0;
+	double cursin = 0.0;
+
+	printf("Введите знаяение N: ");
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++)
+	{
+		cursin += sin(i);
+		sum += 1.0 / cursin;
+	}
+	printf("Результат: %lf\n", sum);
+	getchar();
+	getchar();
+}
